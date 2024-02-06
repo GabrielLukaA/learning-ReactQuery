bf7cb215936047f4bddbbe4d1e7739503d75f9c3
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2024-02-06 11:19:10 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,74 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 
 export default function Home() {
+  const queryClient = useQueryClient(); // É basicamente uma forma manual de refetch, para que eu cojnsiga dar  refetch apenas onde foi atualizado, evitando uma sobrecarga
+  const axios = require("axios").default;
+
+  //Dentro do useQuery como primeiro argumento pode ser passado um array de dois elementos, um que nomeia, e outro que garante que será algo unico
+  //como uma espécie de chave
+  const { data, isLoading, isError, refetch } = useQuery(
+    "tasks",
+    () => {
+      return axios
+        .get("http://localhost:3000/tarefas")
+        .then((response: any) => response.data);
+    },
+    {
+      /*Aqui se passam outros parametroos como por exemplo:
+  retry:5 // Isso faria a query ser feita até 5 vezes caso ela esteja dando erro
+  refetchOnWindowFocus:true // Faz com que sempre que o usuário focar na janela novamente essa query seja refeita
+  refetchInterval: 5000, // É uma forma de fazer a query de tempos em tempos por ms.
+  initialData: [{ id: "1", titulo: "teste" }], // Uma forma de já ter algo sendo mostrado antes de se ter o retorno da api
+  */
+    }
+  );
+
+  // Enquanto o useQuery serve para realizar requisições, o useMutation serve para qualquer tipo de atualização necessária
+  // como um put,post,patch,delete
+  const mutation = useMutation({
+    mutationFn: ({ id, concluida }) => {
+      return axios
+        .patch(`http://localhost:3000/tarefas/${id}`, { concluida })
+        .then((response: any) => response.data);
+    },
+    onSuccess: (data) => {
+      // refetch(); // Essa função básicamente refaz a query!
+      queryClient.setQueryData("tasks", (currentData) =>
+        currentData.map((todo) => (todo.id === data.id ? data : todo))
+      ); // O segundo parametro pode ser uma nova lista diretamente, ou uma função que retorne uma lista
+    },
+    onError: (error) => console.log(error),
+  });
+
+  // mutation.isLoading serve com o mesmo propósito do isLoading do useQuery, para ver se a requisição já foi feita ou não.
+  //Fazendo uso do "initialData" o isLoading sempre será falso, porque sempre terá dados a mostrar.
+  if (isLoading) {
+    return <div className="loading">Carregando...</div>;
+  }
+  if (isError) {
+    return <div className="loading">Algo deu errado!</div>;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="app-container">
+      <div className="todos">
+        <h2>Tasks</h2>
+        {data.map((todo: any) => (
+          <div
+            onClick={() =>
+              mutation.mutate({ id: todo.id, concluida: !todo.concluida })
+            }
+            className={`todo ${todo.concluida && "todo-completed"}`}
+            key={todo.id}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {todo.titulo}
+          </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
